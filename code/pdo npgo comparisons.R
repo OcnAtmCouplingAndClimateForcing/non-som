b8571fd11e6fa162ad940ec84b09b46b821879e8
Mike Litzow <mlitzow@alaska.edu>
2019-06-26 13:29:40 -0800

    Time series plots, acf comparison.

--- code/pdo npgo comparisons.R
+++ code/pdo npgo comparisons.R
@@ -1,6 +1,9 @@
 # compare the distribution / SD / AR1 of 
 # winter pdo and npgo between eras!
 
+library(tidyverse)
+library(zoo)
+
 # download PDO / NPGO and process
 download.file("http://jisao.washington.edu/pdo/PDO.latest", "~pdo")
 names <- read.table("~pdo", skip=30, nrows=1, as.is = T)
@@ -18,18 +21,18 @@ npgo <- read.table("~npgo", skip=10, nrows=828, fill=T, col.names = c("Year", "m
 pdo$win.yr <- ifelse(pdo$month %in% c("NOV", "DEC"), pdo$YEAR+1, pdo$YEAR)
 
 # limit to winter months only
-pdo <- pdo %>%
+win.pdo <- pdo %>%
   filter(month %in% c("NOV", "DEC", "JAN", "FEB", "MAR"))
 
-win.pdo <- tapply(pdo$value, pdo$win.yr, mean)
+win.pdo <- tapply(win.pdo$value, win.pdo$win.yr, mean)
 
 npgo$win.yr <- ifelse(npgo$month %in% 11:12, npgo$Year+1, npgo$Year)
 
 # limit to winter months only
-npgo <- npgo %>%
+win.npgo <- npgo %>%
   filter(month %in% c(11,12,1:3))
 
-win.npgo <- tapply(npgo$value, npgo$win.yr, mean)
+win.npgo <- tapply(win.npgo$value, win.npgo$win.yr, mean)
 
 # and smoothed (2yr) values of each
 npgo2 <- rollapply(win.npgo, 2, mean, align="right", fill=NA)
@@ -59,3 +62,79 @@ ggplot(plot, aes(x=index, y=value, fill=era)) +
 
 ggsave("plots/pdo npgo boxplot.png")
 
+# some other general plots
+ts.plot <- data.frame(year=pdo$YEAR, month=1:12, dec.yr=NA, pdo=pdo$value, npgo=NA)
+ts.plot$dec.yr <- ts.plot$year + (ts.plot$month-0.5)/12
+ts.plot$npgo[601:1428] <- npgo$value
+
+head(ts.plot)
+ts.plot <- ts.plot %>%
+  gather(index, value, -year, -month, -dec.yr)
+
+ts.plot$color <- ifelse(ts.plot$value > 0, "red", "blue")
+ts.plot$index <- reorder(ts.plot$index, desc(ts.plot$index))
+ggplot(ts.plot, aes(dec.yr, value, fill=color)) +
+  theme_linedraw() +
+  geom_bar(stat="identity", color="black", size=0.05) +
+  facet_wrap(~index, nrow=2, scales="free_y") +
+  scale_fill_manual(values=c("blue", "red")) +
+  theme(legend.position = "none", axis.title.x = element_blank()) +
+  xlim(1950,2018)
+
+ggsave("plots/pdo npgo monthly time series.png")
+
+npgo1 <- ts.plot %>%
+  filter(index=="npgo", year %in% 1950:1988)
+ar.npgo1 <- acf(npgo1$value)
+plot(ar.npgo1)
+
+npgo2 <- ts.plot %>%
+  filter(index=="npgo", year %in% 1989:2012)
+ar.npgo2 <- acf(npgo2$value)
+ar.npgo2
+plot(ar.npgo2)
+
+pdo1 <- ts.plot %>%
+  filter(index=="pdo", year %in% 1950:1988)
+ar.pdo1 <- acf(pdo1$value)
+ar.pdo1
+plot(ar.pdo1)
+
+pdo2 <- ts.plot %>%
+  filter(index=="pdo", year %in% 1989:2012)
+ar.pdo2 <- acf(pdo2$value)
+ar.pdo2
+plot(ar.pdo2)
+
+# combine and plot
+ar.plot <- data.frame(lag=rep(1:24, 4), ar=(c(ar.npgo1$acf[2:25], ar.npgo2$acf[2:25], ar.pdo1$acf[2:25], ar.pdo2$acf[2:25])),
+                      index=rep(c("npgo", "pdo"), each=48), era=rep(c("1950-1988", "1989-2012", "1950-1988", "1989-2012"), each=24))
+
+ar.plot$index <- reorder(ar.plot$index, desc(ar.plot$index))
+ggplot(ar.plot, aes(lag, ar, fill=era)) +
+  theme_linedraw() +
+  geom_bar(stat="identity", position="dodge", color="black", size=0.05) +
+  facet_wrap(~index, nrow=2) +
+  ylab("autocorrelation") +
+  theme(legend.position = c(0.8,0.9), legend.title = element_blank())
+
+ggsave("plots/pdo npgo monthly ar by era.png")
+
+# and finally, winters...
+
+plot.win <- data.frame(year=1951:2018, pdo=win.pdo[names(win.pdo) %in% 1951:2018], npgo=win.npgo[names(win.npgo) %in% 1951:2018])
+
+win.plot <- plot.win %>%
+  gather(index, value, -year)
+
+win.plot$color <- as.factor(ifelse(win.plot$value < 0, 1, 2))
+win.plot$index <- reorder(win.plot$index, desc(win.plot$index))
+
+ggplot(win.plot, aes(year, value, fill=color)) +
+  theme_linedraw() +
+  geom_bar(stat="identity", color="black", size=0.05) +
+  facet_wrap(~index, scales="free_y", nrow=2) + 
+  scale_fill_manual(values=c("blue", "red"), labels=c("1950-1988", "1989-2012")) +
+  theme(legend.position = c(0.15,0.92), legend.title = element_blank())
+
+ggsave("plots/winter pdo npgo time series.png")
